70e977590be3789bad1d1ea2a8e9d2e4b253af0d
Taufiq <taufiqabdus@gmail.com>
2017-07-17 18:55:58 +0000

    Finish the gallery, fix signup, add modals

---
+++ script.js
@@ -0,0 +1,49 @@
+/*global google*/ 
+/*global $*/
+/*global href*/
+
+function initMap() {
+        var uluru = {lat: 40.737158, lng: -74.03062339999997};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    
+function carousel(){
+  $(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+}
+carousel();
+$("#nandato").click(function(){
+    $("#fast").hide();
+    $("body").append("<p>"+"Thanks! You're support and feedback helps us alot!"+"</p>")
+});
